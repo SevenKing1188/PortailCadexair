2fe74ac008c9b572c91d06c75f001ee7a09dac27
Guillaume <160023618+SevenKing1188@users.noreply.github.com>
2026-08-17 23:06:21 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const PORT = process.env.PORT || 3000;
 
 app.set('trust proxy', 1);
 
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
 app.use(
   helmet({
     contentSecurityPolicy: {
@@ -22,10 +26,6 @@ app.use(
     }
   })
 );
-
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
-const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 const supabaseAdmin = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY, {
